5041f15824cd3333f3ae09c2aba381576b1d28ab
Pablolizardi <pabloagra3018@gmail.com>
2024-02-21 19:41:27 -0400

    examples of classes

--- classes/class_inheritance.rb
+++ classes/class_inheritance.rb
@@ -21,3 +21,27 @@ class Creature
   drag = Dragon.new("bambi")
   
   drag.fight
+
+
+
+  class Computer
+    @@users = {}
+    def initialize(username, password)
+      @username = username
+      @password = password
+      @files = {}
+      @@users[username] = password
+    end
+    
+    def create(filename)
+      time = Time.now
+      @files[filename] = time
+      puts "#{filename} was created at #{time} by #{@username}. "
+    end
+    def Computer.get_users
+      return @@users
+    end
+  end
+  
+  my_computer = Computer.new("pablos","toiko")
+  
