17d478235964ef7c3e947b32601137fda80eefff
Pablolizardi <pabloagra3018@gmail.com>
2024-02-22 14:02:33 -0400

    custom time module

---
+++ time_module.rb
@@ -0,0 +1,11 @@
+odule ThePresent
+  def now
+    puts "It's #{Time.new.hour > 12 ? Time.new.hour - 12 : Time.new.hour}:#{Time.new.min} #{Time.new.hour > 12 ? 'PM' : 'AM'} (GMT)."
+  end
+end
+
+class TheHereAnd
+  extend ThePresent
+end
+
+TheHereAnd.now
